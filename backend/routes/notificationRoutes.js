3715641ff1ba86393172e6f86e80e47ec33e7156
Dawit-Zeyede <devazeyede10@gmail.com>
2025-01-25 10:53:45 +0300

    Setup

--- backend/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController');
-
-// Send notification (could be for a specific user or event)
-router.post('/send', notificationController.sendUserNotification);
-
-module.exports = router;
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { updateProfile } = require('../controllers/userController');
-const verifyToken = require('../middleware/verifyToken');
-
-// Protected route: Update user profile
-router.put('/user/profile', verifyToken, updateProfile);
-
-module.exports = router;
